41bc584d1dcb439a2e47eceb59366468b31b57fd
Abdelilah04116 <abdelilahourti@gmail.com>
2025-12-14 16:25:40 +0100

    addd

--- Cardio_Federated_System/common/constants.py
+++ Cardio_Federated_System/common/constants.py
@@ -2,3 +2,4 @@
 # Projet : Cardio Federated Learning
 # TODO: Implémentation à venir
 #hhhhhhhhhhhhhhhhhhhhhhhhh
+#sdfghjklcvbnm,vbnm,.khkbljhbcoyusljhbclvctovclcb XLSHGCOWQ  LJH UOYSVFYXS KQX LSICBVS B;WNLUGQWSVLYIILH LHHIWE , CL UUIQLHGCBLCVKQCD LGASVOAHC'AS B ULHAI Lhvjsbgoqbcoinxlja xuozhco ljba
